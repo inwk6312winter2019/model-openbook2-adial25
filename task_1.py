d4de0b5ce679c1e3ef234db647ad57fb78049379
adial25 <Sri.AdilakshmiMulagala@dal.ca>
2019-02-07 23:41:11 -0400

    read file inputs and returning the street name outputs

--- task_1.py
+++ task_1.py
@@ -1,10 +1,48 @@
-file = open("Street_Centrelines.csv","r")
+readFile=open("Street_Centrelines.csv")
+list=[]
+def readData(realFile):
+	readFile.readline()
+	for line in readFile:
+		line=line.split(",")
+		list.append((line[2],line[4],line[6],line[7]))
+		print(list)
+readData(readFile)
 
-'''#task1
-def myfile(file):
-  streetData= []
-  for line in file:
-    line=line.split(",")
-    streetData.append((line[2],line[4],line[7],line[8]))
-  print(streetData)
-myfile(file)
+
+readFile=open("Street_Centrelines.csv")
+def hist(readFile):
+	d = dict()
+	readFile.readline()
+	for line in readFile:
+		line=line.split(",")
+		if line[12]  not in d:
+			d[line[12]] = 1
+		else:
+			d[line[12]] += 1
+	return d
+print(hist(file))
+
+
+file=open("Street_Centrelines.csv")
+def own(file):
+	s=set()
+	file.readline()
+	for line in file:
+		line=line.split(",")
+		s.add(line[11])
+	return s
+print(own(file))
+
+file=open("Street_Centrelines.csv")
+def street(file):
+	z=dict()
+	file.readline()
+	for line in file:
+		line=line.split(",")
+		if line[10]  not in z:
+			z[line[10]] = 1
+		else:
+			z[line[10]] += 1
+	return z
+
+print(street(file))
